b19c771a9adef474ff5a3fbaee0269adfe623e2c
Enitoni <enitoni@email.com>
2018-11-28 15:18:59 +0100

    add service

---
+++ src/service/classes/Service.ts
@@ -0,0 +1,42 @@
+import { Bot } from "../../bot/classes"
+
+export class Service<M> {
+  private didStart = false
+
+  protected bot: Bot<M>
+
+  constructor(bot: Bot<M>) {
+    this.bot = bot
+  }
+
+  public async _initialize() {
+    await this.serviceDidInitialize()
+  }
+
+  public async _start() {
+    if (this.didStart) {
+      await this.servicdeDidRestart()
+    } else {
+      this.didStart = true
+      await this.serviceDidStart()
+    }
+  }
+
+  public async _stop() {
+    await this.serviceDidStop()
+  }
+
+  /** Hook called when the service has initialized, but the bot is ready */
+  protected async serviceDidInitialize() {}
+
+  /** Hook called when the service has started and the bot is ready */
+  protected async serviceDidStart() {}
+
+  /** Hook called when the service has started after stopping */
+  protected async servicdeDidRestart() {}
+
+  /** Hook called when the service has stopped and the bot is offline */
+  protected async serviceDidStop() {}
+}
+
+export type ServiceClass<M> = new (bot: Bot<M>) => Service<M>
