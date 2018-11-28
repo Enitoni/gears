07e605f09ede3c8bea2507e16067c11d472da6fa
Enitoni <enitoni@email.com>
2018-11-28 10:17:55 +0100

    use void here

--- src/command/types/CommandMatcher.ts
+++ src/command/types/CommandMatcher.ts
@@ -1,3 +1,3 @@
 import { PromiseResolvable } from "../../core/types"
 
-export type CommandMatcher<C> = (context: C) => PromiseResolvable<C | undefined>
+export type CommandMatcher<C> = (context: C) => PromiseResolvable<C | void>
