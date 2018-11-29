7bfa4580a6866b942dd7d5b7d721aec1873ba49d
Enitoni <enitoni@email.com>
2018-11-29 18:18:02 +0100

    resolve error message

--- src/command/classes/CommandError.ts
+++ src/command/classes/CommandError.ts
@@ -1,10 +1,11 @@
 import { Context, MatchResult } from "../types"
 
 export class CommandError<M> extends Error {
+  name = "CommandError"
   public match: MatchResult<Context<unknown, M>>
 
   constructor(match: MatchResult<Context<unknown, M>>, error: any) {
-    super(error)
+    super(error.message || error)
     this.match = match
   }
 }
