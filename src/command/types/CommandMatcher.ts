64dadb786fdf3fbc1f0197c9f93557ceeabac206
Crawron <azertyaaron@gmail.com>
2019-07-03 15:59:50 -0500

    Fixed CommandMatcher generics

--- src/command/types/CommandMatcher.ts
+++ src/command/types/CommandMatcher.ts
@@ -1,6 +1,6 @@
 import { PromiseResolvable } from "../../core/types"
 import { Context } from "./Context"
 
-export type CommandMatcher<D = {}, M = any, C = any> = (
+export type CommandMatcher<D = any, M = any, C = any> = (
   context: Context<{}, M, C>
 ) => PromiseResolvable<Context<D, M, C> | void>
