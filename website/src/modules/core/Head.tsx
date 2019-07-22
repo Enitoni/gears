9e1820983bca6c431a45a3d3cf2d449e5f5e0a4e
Enitoni <enitoni@email.com>
2019-07-22 19:23:47 +0200

    Clear server side head

--- website/src/modules/core/Head.tsx
+++ website/src/modules/core/Head.tsx
@@ -1,30 +1,40 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { IS_SERVER } from "./constants"
 import ReactDOM from "react-dom"
 
 const IMPORTED_CHARACTERS = "ABCDEFGHIKLMNOPQRSTUVXYZabcdefghiklmnopqrstuvxyz"
 
 export function Head() {
-  const content = (
-    <>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1" />
-      <title>Gears</title>
-      <link
-        href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:700&display=swap&text=Gears"
-        rel="stylesheet"
-      />
-      <link
-        href={`https://fonts.googleapis.com/css?family=Barlow:400,600,700&display=swap&text=${IMPORTED_CHARACTERS}`}
-        rel="stylesheet"
-      />
-    </>
+  const content = [
+    <meta charSet="utf-8" />,
+    <meta name="viewport" content="initial-scale=1" />,
+    <title>Gears</title>,
+    <link
+      href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:700&display=swap&text=Gears"
+      rel="stylesheet"
+    />,
+    <link
+      href={`https://fonts.googleapis.com/css?family=Barlow:400,600,700&display=swap&text=${IMPORTED_CHARACTERS}`}
+      rel="stylesheet"
+    />
+  ].map((element, i) =>
+    React.cloneElement(element, {
+      key: i
+    })
   )
+
+  useEffect(() => {
+    if (IS_SERVER) return
+
+    document
+      .querySelectorAll("[data-server-head=true]")
+      .forEach(element => element.remove())
+  })
 
   if (IS_SERVER) {
     return (
       <>
-        {React.Children.map(content, element =>
+        {content.map(element =>
           React.cloneElement(element, {
             "data-server-head": true
           })
