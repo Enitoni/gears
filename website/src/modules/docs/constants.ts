748262d9c7a1b82dbed0eecdf174dadcca69cf30
Enitoni <enitoni@email.com>
2019-07-23 23:23:46 +0200

    add Type Alias kind

--- website/src/modules/docs/constants.ts
+++ website/src/modules/docs/constants.ts
@@ -4,5 +4,6 @@ import { IconType } from "../../common/icon/types/IconType"
 export const kindToIconMap: Record<ModuleKind, IconType> = {
   Class: "encapsulated",
   Function: "doubleParens",
-  Interface: "placeholder"
+  Interface: "placeholder",
+  "Type alias": "placeholder"
 }
--- website/src/modules/docs/types/ModuleDescriptor.ts
+++ website/src/modules/docs/types/ModuleDescriptor.ts
@@ -1,6 +1,6 @@
 import { Descriptor } from "./Descriptor"
 
-export type ModuleKind = "Class" | "Function" | "Interface"
+export type ModuleKind = "Class" | "Function" | "Interface" | "Type alias"
 
 export interface ModuleDescriptor<T extends ModuleKind> extends Descriptor<T> {
   name: string
