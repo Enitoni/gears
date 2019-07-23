a521e68a099cd98b21aac21cad616770cc791f33
Enitoni <enitoni@email.com>
2019-07-23 17:33:59 +0200

    Make sidebar independent

--- website/src/common/navigation/components/Sidebar.tsx
+++ website/src/common/navigation/components/Sidebar.tsx
@@ -1,19 +1,40 @@
 import { styled } from "../../../modules/theming/themes"
 import React from "react"
+import { HEADER_HEIGHT } from "../../../modules/core/components/Header"
 
 export interface SidebarProps {
   children: React.ReactNode
 }
 
 const Container = styled.div`
+  position: fixed;
+  top: ${HEADER_HEIGHT};
+  bottom: 0px;
+
   flex-shrink: 0;
-
   width: 284px;
-  margin-right: 16px;
+  padding-top: 32px;
 
-  padding-bottom: 64px;
+  overflow-y: scroll;
+`
+
+const Space = styled.div`
+  width: 284px;
+  margin-right: 32px;
+`
+
+const BottomSpace = styled.div`
+  height: 32px;
 `
 
 export function Sidebar(props: SidebarProps) {
-  return <Container>{props.children}</Container>
+  return (
+    <>
+      <Container>
+        {props.children}
+        <BottomSpace />
+      </Container>
+      <Space />
+    </>
+  )
 }
