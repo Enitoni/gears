3cf1f46adae3e51360230628882a1ac6267ec301
Enitoni <enitoni@email.com>
2019-07-24 00:29:53 +0200

    parse [[Module]] in Description

--- website/src/modules/docs/components/DescriptorRenderer/Description.tsx
+++ website/src/modules/docs/components/DescriptorRenderer/Description.tsx
@@ -1,6 +1,8 @@
 import { ModuleDescriptor, ModuleKind } from "../../types/ModuleDescriptor"
 import { styled } from "../../../theming/themes"
 import React from "react"
+import { Link } from "../../../../common/navigation/components/Link"
+import { ModuleLink } from "../ModuleLink"
 
 export interface DescriptionProps {
   descriptor: ModuleDescriptor<ModuleKind>
@@ -12,6 +14,27 @@ const Container = styled.div`
 
 export function Description(props: DescriptionProps) {
   const { descriptor } = props
+  const { description } = descriptor
 
-  return <Container>{descriptor.description}</Container>
+  if (!description) return null
+
+  const parts = description.split(/\[\[\w+]]/g)
+  const matches = description.match(/\[\[\w+]]/g)
+
+  if (!matches) return <Container>{description}</Container>
+
+  let finalDescription = []
+
+  for (const [i, part] of parts.entries()) {
+    finalDescription.push(part)
+
+    if (i === matches.length) break
+
+    const match = matches[i]
+    const [, moduleName] = /\[\[(\w+)]]/.exec(match)!
+
+    finalDescription.push(<ModuleLink key={`${moduleName}-${i}`} name={moduleName} />)
+  }
+
+  return <Container>{finalDescription}</Container>
 }
