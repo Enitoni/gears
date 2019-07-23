3e8cb7a18542fff4104ca3a22c2cd189dbc86568
Enitoni <enitoni@email.com>
2019-07-23 23:53:25 +0200

    add Link component

---
+++ website/src/common/navigation/components/Link.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+import { styled } from "../../../modules/theming/themes"
+import { getFontColor, getColor, getDuration } from "../../../modules/theming/helpers"
+import { useRouteLink } from "../../routing/hooks/useRouteLink"
+
+const Container = styled.a`
+  color: ${getFontColor("muted")};
+
+  transition: ${getDuration("normal")} ease;
+  transition-property: color;
+
+  &:hover {
+    ${getColor("accent")};
+  }
+`
+
+export interface LinkProps {
+  to: string
+  children?: React.ReactNode
+}
+
+export function Link(props: LinkProps) {
+  const { to, children } = props
+  const [_, click] = useRouteLink(to)
+
+  return (
+    <Container href={to} onClick={click}>
+      {children}
+    </Container>
+  )
+}
