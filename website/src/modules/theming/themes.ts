12fa455ba86ae6c777464f1c799e16a29d92e625
Enitoni <enitoni@email.com>
2019-07-23 17:33:18 +0200

    add lightTheme

--- website/src/modules/theming/themes.ts
+++ website/src/modules/theming/themes.ts
@@ -12,7 +12,25 @@ export const darkTheme: Theme = {
   },
   transparencies: {
     positive: "rgba(78, 75, 125, 0.5)",
-    negative: "rgba(0, 0, 0, 0.3)"
+    negative: "rgba(0, 0, 0, 0.2)"
+  },
+  durations: {
+    normal: "200ms"
+  }
+}
+
+export const lightTheme: Theme = {
+  colors: {
+    primary: "#eeeeee",
+    accent: "#e81e57"
+  },
+  fontColors: {
+    normal: "#29274c",
+    muted: "rgba(0, 0, 0, 0.6)"
+  },
+  transparencies: {
+    positive: "rgba(0, 0, 0, 0.2)",
+    negative: "rgba(0, 0, 0, 0.2)"
   },
   durations: {
     normal: "200ms"
