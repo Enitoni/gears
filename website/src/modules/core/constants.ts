672d6c798ef2c448e1d00b60dc6ef884d0766945
Enitoni <enitoni@email.com>
2019-07-24 01:03:34 +0200

    fix padding

--- website/src/modules/core/constants.ts
+++ website/src/modules/core/constants.ts
@@ -1,8 +1,8 @@
 export const BUILD_FOLDER = "./build"
 export const BUILD_PUBLIC_FOLDER = "./build/public"
 
-export const MAX_BODY_WIDTH = "1230px"
-export const BODY_PADDING = "16px"
+export const MAX_BODY_WIDTH = "1260px"
+export const BODY_PADDING = "32px"
 
 export const DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === "development"
 export const IS_SERVER = process.env.__SERVER__ === "true"
