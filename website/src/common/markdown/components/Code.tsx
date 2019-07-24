1309263d754208a310c3adfa69a30c58d7dd3fa0
Enitoni <enitoni@email.com>
2019-07-24 23:50:18 +0200

    Tweak code style

--- website/src/common/markdown/components/Code.tsx
+++ website/src/common/markdown/components/Code.tsx
@@ -4,7 +4,8 @@ import React from "react"
 
 const Container = styled.pre`
   font-family: Fira Mono, monospace;
-  font-size: 14px;
+  font-size: 15px;
+  line-height: 21px;
 
   background: ${getTransparency("negative")};
   border-radius: 3px;
