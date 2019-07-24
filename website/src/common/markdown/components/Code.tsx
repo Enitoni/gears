318d4047ed73db745303fb6e8ac7ecea7760bb79
Enitoni <enitoni@email.com>
2019-07-24 23:41:59 +0200

    add Code component

---
+++ website/src/common/markdown/components/Code.tsx
@@ -0,0 +1,17 @@
+import { styled } from "../../../modules/theming/themes"
+import { getTransparency } from "../../../modules/theming/helpers"
+import React from "react"
+
+const Container = styled.pre`
+  font-family: Fira Mono, monospace;
+  font-size: 14px;
+
+  background: ${getTransparency("negative")};
+  border-radius: 3px;
+
+  padding: 16px;
+`
+
+export function Code(props: { children: React.ReactNode }) {
+  return <Container>{props.children}</Container>
+}
