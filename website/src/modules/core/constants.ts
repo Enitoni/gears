77f3874931f51428eba3ac0634fbebdca5f54afa
Enitoni <enitoni@email.com>
2019-07-25 19:05:15 +0200

    Increase body width

--- website/src/modules/core/constants.ts
+++ website/src/modules/core/constants.ts
@@ -4,7 +4,7 @@ import { Transform } from "stream"
 export const BUILD_FOLDER = "./build"
 export const BUILD_PUBLIC_FOLDER = "./build/public"
 
-export const MAX_BODY_WIDTH = "1260px"
+export const MAX_BODY_WIDTH = "1270px"
 export const BODY_PADDING = "32px"
 
 export const DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === "development"
