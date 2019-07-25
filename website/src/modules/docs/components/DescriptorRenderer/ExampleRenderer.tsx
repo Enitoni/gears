0dc0bfea6dd4ef6cb539b38b33da8552eaeb3799
Enitoni <enitoni@email.com>
2019-07-25 03:42:33 +0200

    add ExampleRenderer

---
+++ website/src/modules/docs/components/DescriptorRenderer/ExampleRenderer.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { ModuleDescriptor } from "../../types/ModuleDescriptor"
+import { Section } from "../../../../common/navigation/components/Section"
+import { Code } from "../../../../common/markdown/components/Code"
+
+export interface ExampleRendererProps {
+  descriptor: ModuleDescriptor
+}
+
+export function ExampleRenderer(props: ExampleRendererProps) {
+  const { descriptor } = props
+
+  if (!descriptor.example) return null
+
+  return (
+    <Section title="Example">
+      <Code>{descriptor.example}</Code>
+    </Section>
+  )
+}
