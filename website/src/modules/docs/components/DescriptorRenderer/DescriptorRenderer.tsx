17e8bcd965e5a19204d9995cffae224209d232ae
Enitoni <enitoni@email.com>
2019-07-25 03:42:26 +0200

    expand

--- website/src/modules/docs/components/DescriptorRenderer/DescriptorRenderer.tsx
+++ website/src/modules/docs/components/DescriptorRenderer/DescriptorRenderer.tsx
@@ -5,12 +5,15 @@ import { styled } from "../../../theming/themes"
 import { Description } from "./Description"
 import { InheritingRenderer } from "./InheritingRenderer"
 import { Inheriting } from "../../types/Inheriting"
+import { ExampleRenderer } from "./ExampleRenderer"
 
 export interface DescriptorRendererProps {
   descriptor: ModuleDescriptor
 }
 
 const Container = styled.div`
+  flex: 1;
+
   > .content {
     margin-top: 16px;
     margin-left: ${ICON_SPACE};
@@ -26,6 +29,7 @@ export function DescriptorRenderer(props: DescriptorRendererProps) {
       <div className="content">
         <InheritingRenderer descriptor={descriptor as Inheriting} />
         <Description descriptor={descriptor} />
+        <ExampleRenderer descriptor={descriptor} />
       </div>
     </Container>
   )
