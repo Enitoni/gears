f91ebefa279b354efc0cc72566e4fc435c9a8a7b
Enitoni <enitoni@gmail.com>
2019-07-30 02:37:15 +0200

    fix homepage link

--- website/src/modules/core/components/HomePage.tsx
+++ website/src/modules/core/components/HomePage.tsx
@@ -3,8 +3,12 @@ import { Heading } from "../../../common/markdown/components/Heading"
 import React from "react"
 import { Paragraph } from "../../../common/markdown/components/Paragraph"
 import { Link } from "../../../common/navigation/components/Link"
+import { useStores } from "../../../common/state/hooks/useStores"
 
 export function HomePage() {
+  const { documentationStore } = useStores()
+  const { latestVersion } = documentationStore
+
   return (
     <Content>
       <Heading icon="home">Home</Heading>
@@ -13,7 +17,7 @@ export function HomePage() {
           "Gears is a library used to create command interfaces, such as chat bots and more."
         }
       </Paragraph>
-      <Link to={"/docs"}>Check out the documentation</Link>
+      <Link to={`/docs/${latestVersion}`}>Check out the documentation</Link>
     </Content>
   )
 }
