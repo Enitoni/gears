7dd9575d878aed9eef3421b89414c79d9ddaec8b
Enitoni <enitoni@gmail.com>
2019-07-30 10:20:25 +0200

    add CommandLine component

---
+++ website/src/modules/core/demo/components/CommandLine.tsx
@@ -0,0 +1,120 @@
+import { styled } from "../../../theming/themes"
+import React, { useState, FormEvent, useRef, useLayoutEffect } from "react"
+import {
+  getSyntaxColor,
+  getColor,
+  getTransparency,
+  getFontColor
+} from "../../../theming/helpers"
+import { darken } from "polished"
+
+export interface CommandLineEntry {
+  content: string
+  type: "input" | "response"
+}
+
+export interface CommandLineProps {
+  entries: CommandLineEntry[]
+  onLine: (message: string) => void
+}
+
+const Container = styled.div`
+  padding: 0px 24px;
+`
+
+const Entries = styled.div`
+  margin: 0px -24px;
+
+  padding: 24px;
+  padding-bottom: 38px;
+
+  background: ${getSyntaxColor("background")};
+  border-radius: 3px;
+
+  max-height: 350px;
+  overflow-y: auto;
+`
+
+const Line = styled.div<{ response: boolean }>`
+  font-family: Fira Code, monospace;
+  font-size: 1em;
+
+  & ~ div {
+    margin-top: 8px;
+  }
+
+  color: ${({ response, theme }) =>
+    response ? theme.colors.accent : theme.syntaxColors.comment};
+
+  ::before {
+    content: ${props => (props.response ? `"«"` : `"»"`)};
+    display: inline-block;
+    margin-right: 8px;
+  }
+`
+
+const Input = styled.input`
+  position: relative;
+
+  font-size: 1em;
+  font-family: Fira Code, monospace;
+
+  display: block;
+
+  padding: 8px;
+  margin-top: -24px;
+
+  background: ${props => darken(0.05, props.theme.colors.primary)};
+  border: solid 3px ${getColor("primary")};
+  border-radius: 3px;
+
+  width: 100%;
+  color: ${getFontColor("normal")};
+
+  outline: none;
+
+  &::placeholder {
+    color: ${getSyntaxColor("comment")};
+  }
+`
+
+export function CommandLine(props: CommandLineProps) {
+  const { entries, onLine } = props
+
+  const entryRef = useRef<HTMLDivElement>(null)
+  const [value, setValue] = useState("")
+
+  const submit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    onLine(value)
+    setValue("")
+  }
+
+  useLayoutEffect(() => {
+    const { current: element } = entryRef
+
+    if (element) {
+      element.scrollTo(0, element.clientHeight)
+    }
+  }, [entries.length])
+
+  return (
+    <Container>
+      <Entries ref={entryRef}>
+        {entries.map((entry, i) => (
+          <Line response={entry.type === "response"} key={i}>
+            {entry.content}
+          </Line>
+        ))}
+      </Entries>
+      <form onSubmit={submit}>
+        <Input
+          placeholder="Type a message"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+    </Container>
+  )
+}
