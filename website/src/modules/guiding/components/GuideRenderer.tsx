e7ee12dd24fda60f341042ca8f7b388332b1a24f
Enitoni <enitoni@gmail.com>
2019-07-31 20:47:47 +0200

    add subtitle

--- website/src/modules/guiding/components/GuideRenderer.tsx
+++ website/src/modules/guiding/components/GuideRenderer.tsx
@@ -3,10 +3,16 @@ import { useMeta } from "../../core/hooks/useMeta"
 import React from "react"
 import { Heading } from "../../../common/markdown/components/Heading"
 import { Content } from "../../../common/markdown/components/Content"
+import { styled } from "../../theming/themes"
 
 export interface GuideRendererProps {
   guide: Guide
 }
+
+const Subtitle = styled.h2`
+  margin-top: -8px;
+  margin-bottom: 16px;
+`
 
 export function GuideRenderer(props: GuideRendererProps) {
   const { guide } = props
@@ -20,6 +26,7 @@ export function GuideRenderer(props: GuideRendererProps) {
   return (
     <Content>
       <Heading icon="book">{title}</Heading>
+      <Subtitle>{description}</Subtitle>
       {render()}
     </Content>
   )
