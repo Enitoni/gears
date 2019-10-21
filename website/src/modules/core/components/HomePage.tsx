32ad6a5fd8d0549a9d5d57471abf62ced082e88a
Enitoni <enitoni@gmail.com>
2019-10-21 21:01:58 +0200

    add repo link to HomePage

--- website/src/modules/core/components/HomePage.tsx
+++ website/src/modules/core/components/HomePage.tsx
@@ -7,6 +7,7 @@ import { useStores } from "../../../common/state/hooks/useStores"
 import { Section } from "../../../common/markdown/components/Section"
 import { Demonstration } from "../../demo/components/Demonstration"
 import { Code } from "../../../common/markdown/components/Code"
+import { ExternalLink } from "../../../common/navigation/components/ExternalLink"
 
 export function HomePage() {
   const { documentationStore } = useStores()
@@ -21,6 +22,13 @@ export function HomePage() {
         <Link to={`/guides/installation`}>Get started here</Link>
         {" or "}
         <Link to={`/docs/${latestVersion}`}>Check out the documentation</Link>
+      </Paragraph>
+      <Paragraph>
+        Also check out{" "}
+        <ExternalLink to="https://gitlab.com/enitoni-gears/gears/tree/master/gears">
+          the repository
+        </ExternalLink>{" "}
+        if you are curious or have any issues you want to address.
       </Paragraph>
       <Section icon="generic" title="Demo">
         <Demonstration />
