afbce0e28559a1d544c1c5bee25de23be20ad3c5
Enitoni <enitoni@gmail.com>
2020-03-19 11:08:15 +0100

    update index

--- gears/src/index.ts
+++ gears/src/index.ts
@@ -1,4 +1,11 @@
-export * from "./bot"
-export * from "./command"
-export * from "./core"
-export * from "./service"
+import * as bot from "./bot"
+import * as service from "./service"
+import * as command from "./command"
+
+export * from "./command/matchers"
+
+export const core = {
+  ...bot,
+  ...service,
+  ...command,
+}
